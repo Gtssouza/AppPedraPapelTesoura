2aa53d01f89122b212eb4995d25e0d8b4c577988
Gustavo Souza <gtssouza94@gmail.com>
2020-07-01 18:09:50 -0300

    Criado onclick e metodos da interface

--- app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/example/apppedrapapeltesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.apppedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view){
+        System.out.println("Opção Pedra selecionada");
+        this.opcaoSelecionada(1);
+    }
+
+    public void selecionadoPapel(View view){
+        System.out.println("Opção Papel selecionada");
+        this.opcaoSelecionada(2);
+    }
+
+    public void selecionadoTesoura(View view){
+        System.out.println("Opção Tesoura selecionada");
+        this.opcaoSelecionada(3);
+    }
+
+    public void opcaoSelecionada(int opçãoSelecionada){
+        System.out.println(opçãoSelecionada);
     }
 }
